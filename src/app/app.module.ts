e5ed08529eeb18600bc1c286c635051d1700cd31
Natalya <natsik94@mail.ru>
2018-07-22 21:59:09 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InformationComponent } from './information/information.component';
+import { PlanComponent } from './plan/plan.component';
+import { BillingComponent } from './billing/billing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InformationComponent,
+    PlanComponent,
+    BillingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/billing/billing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billing',
+  templateUrl: './billing.component.html',
+  styleUrls: ['./billing.component.less']
+})
+export class BillingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
